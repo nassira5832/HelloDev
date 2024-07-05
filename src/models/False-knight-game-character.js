ca630db66d35031db002ea39a1107eca82fabc9a
nassira5832 <nassira.aitmehdi@gmail.com>
2024-07-05 05:27:53 +0200

    le schéma

---
+++ src/models/False-knight-game-character.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const characterSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    level:{
+        type: Number,
+        required: true
+    }
+});
+const character= mongoose.model("character",characterSchema);
+module.exports = { Character };
---
+++ src/routes/False knight.js
@@ -0,0 +1,6 @@
+const router= express.router;
+
+router.post("/False knight",(req,res)=>{
+      const {nom , level }=req.body;
+      
+})
