e2d10e1892c3b90339e3eac2180441d2e58559b4
nassira5832 <nassira.aitmehdi@gmail.com>
2024-07-05 11:54:19 +0200

    la methode post

--- src/routes/False knight.js
+++ src/routes/False knight.js
@@ -1,6 +1,22 @@
 const router= express.router;
+const character = require ("../models/False-knight-game-character");
+
 
 router.post("/False knight",(req,res)=>{
-      const {nom , level }=req.body;
+      const {name , level }=req.body;
+      if (!name || !level) {
+            res.json("Name and level are required ")
+      }
+      try {
+            const newCharacter= new character ({
+                  name: name,
+                  level: level
+            })
+        newCharacter.save(); 
+        res.json("Character saved successfully");
+
+      } catch (error) {
+            console.log("Error while creating the character: ", error);
+      }
       
 })
